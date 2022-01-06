fff959e24e8f99d407421a5234d830a298e88aa3
weedl <94055286+weedl@users.noreply.github.com>
2022-01-06 12:38:08 +0100

    Add files via upload

---
+++ SetTest.py
@@ -0,0 +1,6 @@
+my_fav_fastfood = {"McDonalds", "Burger King", "Subway", "KFC", "Wendys"} # A set of my fav fastfood
+friends_fav_fastfood = {"McDonalds", "DunkinDonuts", "KFC", "BigBite", "FiveGuys"} # A set of my friends fav fastfood
+
+my_fav_fastfood.add("Pizzahut")
+
+print(my_fav_fastfood.intersection(friends_fav_fastfood)) # Finds the intersection - the values that the sets have in common
