017d84cccc205847f1e781ac8d294c62016641ae
weedl <94055286+weedl@users.noreply.github.com>
2022-01-09 22:11:07 +0100

    Update MetricConverter.py
    Changed the print line to make the output cleaner

--- MetricConverter.py
+++ MetricConverter.py
@@ -2,7 +2,7 @@ def metric_converter():  # Creates a function without a variable
     x = int(input("Enter a amount of inches you would\nlike to convert to centimeters: "))
     # Asks the user to input an amount of inches to convert. The script turns the input into an integer
 
-    print("You entered " + str(x) + " inches, which equals to " + str(x * 0.393700787) + " centimeters")
+    print("\nThe number of inches entered: " + str(x) + " \n\nEquals to: " + str(x * 0.393700787) + "cm")
     # Tells the user information, and adds the formula for converting inches into centimeters,
     # and thereafter turning the result into string
 
