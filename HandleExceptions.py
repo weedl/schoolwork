742a8df0df08fb1fbc10774ed9b7315acbb26573
weedl <94055286+weedl@users.noreply.github.com>
2022-01-11 17:14:29 +0100

    Update HandleExceptions.py

--- HandleExceptions.py
+++ HandleExceptions.py
@@ -12,11 +12,19 @@ while 1:  # starts a while loop so that the user can add values until it breaks
     else:
         print("\nThat input is invalid!\n")
 
-print(list_values)  # gives the user an overview of the values entered
-number = int(input("Please enter how many values you would like to add together: "))
+        
+try:
+    number = int(input("Please enter how many values you would like to add together: "))
+    if type(number) is int:
+        print("Calculating")
+    else:
+        raise ValueError("Ouch, bad choice, that is not a number")
+except ValueError as e:
+    print(e)
+    exit(1)
 # asks the user how many of the values he or she wants to add together
-
-
+ 
+    
 sales_list = list_values[:number]  # creates a new list with the values the user wants to add together
 sales_total = sum(sales_list)  # creates a sales total consisting of the sum of 'sales_list' values
 print(str(sales_total) + "kr")  # displays the sales total to the user
